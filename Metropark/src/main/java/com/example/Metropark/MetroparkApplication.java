377040bc80b557974067e1e7225b95ebf4dddbff
vijayguhan10 <vijayguhan10@gmail.com>
2026-06-24 15:33:41 +0530

    Updated with the Initial Configuration of the Application

---
+++ Metropark/src/main/java/com/example/Metropark/MetroparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Metropark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroparkApplication.class, args);
+	}
+
+}
---
+++ Metropark/src/test/java/com/example/Metropark/MetroparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Metropark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
